7602ff9934d93acc708c756411f325ce6b5ca392
Mahsa Hassankashi <mahsa.hassankashi@gmail.com>
2019-05-11 07:26:45 -0700

    Update settings.py

--- smengine_backend/settings.py
+++ smengine_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'tx3v6hgijcw=z+dr_zeen*=_fp-sc2xhxlvt5$et+4ue*oa$%u'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','*']
 
 
 # Application definition
